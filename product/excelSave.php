11d04b30e347be58747f5462e3f89c290baddbbf
hyoukhoon <hyoukhoon@gmail.com>
2021-06-18 17:34:56 +0900

    ㄹㅇㅁㄹ

--- product/excelSave.php
+++ product/excelSave.php
@@ -452,7 +452,8 @@ header ('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 header ('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT'); // always modified
 header ('Cache-Control: cache, must-revalidate'); // HTTP/1.1
 header ('Pragma: public'); // HTTP/1.0
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
+ob_end_clean();
 $objWriter->save('php://output');
 exit;
 
